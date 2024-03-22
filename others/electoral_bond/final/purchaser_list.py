d7a95e0e2ec4acb873504d9b11cea4bccf7db575
piappstudio <piappstudio@gmail.com>
2024-03-22 12:54:30 -0500

    Final List and script has been updated

--- others/electoral_bond/final/purchaser_list.py
+++ others/electoral_bond/final/purchaser_list.py
@@ -3,31 +3,26 @@ import pandas as pd
 import re
 
 def parse_line(line):
-    # Define regular expressions for the patterns
+    prefixs = ['OT','TT', 'OL', 'TL', 'OC']  # List of names to check against
+
+
     print("-------------------------------------------------------------")
     print(line)
     parts = line.split()
-    print(len(parts))
-
-    sliced_array = parts[2:]
-    account_no_index = 0
+    sliced_array = parts[5:]
+    prefix_index = 5
     party_name = ""
     for index, element in enumerate(sliced_array):
-        if not element.startswith('*'):
+        if not any (element.startswith(deno) for deno in prefixs):
             party_name+= element+" "
         else:
-            account_no_index = index
+            prefix_index += index
             break
+    print("End of loop")
+    print(prefix_index)
+    prefix = parts[prefix_index]
 
-    account_no_index+=2
-    acc_no = parts[account_no_index]
-    prefix = parts[account_no_index+1]
-    bond_number = parts[account_no_index+2]
-    amount = parts[account_no_index+3].replace(',', "")
-    branch_code = parts[account_no_index+4]
-    pay_teller = parts[account_no_index+5]
-
-    return parts[0], parts[1], party_name.strip(), acc_no, prefix, bond_number, amount, branch_code, pay_teller
+    return parts[0], parts[1], parts[2], parts[3], parts[4], party_name.strip(), prefix, parts[prefix_index+1], parts[prefix_index+2].replace(",", ""),parts[prefix_index+3], parts[prefix_index+4],parts[prefix_index+5]
 
 def extract_table_from_pdf(pdf_path, start_page, end_page):
     table_data = []
@@ -37,18 +32,16 @@ def extract_table_from_pdf(pdf_path, start_page, end_page):
         for page in pdf_reader.pages:
 
             text = page.extract_text()
-            
-            title = ["Sr", "EncashmentName", "Political", 'PartyPrefixBond', 'NumberDenominationsPay', 'Branch', 'CodePay', 'Teller', 'Page']  # List of names to check against
             # Split text into lines and extract table-like data
             lines = text.split('\n')
             for line in lines:
-                if len(line.split()) >= 8:  # Assuming at least 6 characters for each line
+                if len(line.split()) >= 10:  # Assuming at least 6 characters for each line
                     table_data.append(parse_line(line))           
             
     return table_data
 
 def save_to_excel(table_data, output_path):
-    df = pd.DataFrame(table_data, columns=['Sr.No', 'Date of Encashment', 'Name of Political Party', "Account no. political party",'Prefix', 'Bond Number', 'Denominations', 'Pay Branch Code', 'Pay Teller' ])
+    df = pd.DataFrame(table_data, columns=['Sr.No', 'Reference No (URN)', 'Journal Date', "Date Of Purchase",'Date of Expiry','Name of the Purchaser','Prefix', 'Bond Number', 'Denominations', 'Issue Branch Code', 'Issue Teller', 'Status'])
     df.to_excel(output_path, index=False)
 
 def convert_pdf_to_excel(pdf_path, start_page, end_page, output_path):
@@ -56,9 +49,9 @@ def convert_pdf_to_excel(pdf_path, start_page, end_page, output_path):
     save_to_excel(table_data, output_path)
 
 # Example usage
-pdf_path = 'party_list.pdf'  # Path to your PDF file
+pdf_path = 'purchaser_list.pdf'  # Path to your PDF file
 start_page = 1  # Start page of the table
 end_page = 3  # End page of the table
-output_path = 'party_list.xlsx'  # Path where the Excel file will be saved
+output_path = 'purchaser_list.xlsx'  # Path where the Excel file will be saved
 
 convert_pdf_to_excel(pdf_path, start_page, end_page, output_path)
